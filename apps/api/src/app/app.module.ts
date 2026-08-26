3a20d7aede029ae93a904312dfc2d3019b767a3c
Yamen Shahin <yamenshahin@gmail.com>
2026-08-26 11:24:03 +0300

    Add .env file for API configuration with MongoDB URI and port

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,9 +1,34 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
+import { join } from 'path';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        uri: config.getOrThrow<string>('MONGO_URI'),
+      }),
+    }),
+    GraphQLModule.forRootAsync<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      useFactory: () => ({
+        autoSchemaFile: join(process.cwd(), 'apps/api/src/schema.gql'),
+        sortSchema: true,
+        playground: false, // disable deprecated GraphQL Playground
+        plugins: [ApolloServerPluginLandingPageLocalDefault()], // Apollo Sandbox
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
